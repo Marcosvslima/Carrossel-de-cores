d43ec52ade21eb265350cce56154f384723e2cba
marcos <souzalimam166@gmail.com>
2024-03-27 17:39:58 -0300

    mudando o estilo e a interatividade

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let listaDeCores = ["blue","red","green","yellow","black","pink","brown","purple","orange"]
 
 let btn = document.getElementById("btn")
-let divi = document.getElementById("divi")
+let span = document.getElementById("span")
 
 btn.addEventListener("click" ,function (){
     number = getNumberRandom()
     document.body.style.backgroundColor = listaDeCores[number]
-    divi.innerHTML = listaDeCores[number]
+    span.innerHTML = listaDeCores[number]
 })
 function getNumberRandom(){
    return parseInt( Math.random() * listaDeCores.length)
